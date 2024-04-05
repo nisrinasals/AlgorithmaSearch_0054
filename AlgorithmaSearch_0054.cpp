e346e9085f0da929f2f66696d2b3d6f0636668f4
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-05 15:11:42 +0700

    menuliskan step 4

--- AlgorithmaSearch_0054.cpp
+++ AlgorithmaSearch_0054.cpp
@@ -40,7 +40,9 @@ void LinearSearch(){
             ctr++;
             if (arr[i] == item){
                 cout << "\n" << item << "found at position " << (i+1) << endl;
+                break;
             }
+            i++;
         }
 
     }
